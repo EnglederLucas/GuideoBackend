32a9f70f32fbd88a1ec764b16b2422a2ba5eea26
Alexander Leeb <alexander.leeb11@gmail.com>
2020-02-24 15:15:25 +0100

    First firebase init

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application } from "express";
 import GuideoServer from "./application/GuideoServer";
 import {GuideController} from "./logic/controllers";
 import { UnitOfWork } from './persistence/inmemory/unitofwork';
+import * as admin from 'firebase-admin';
 
 const port: number = 3030;
 const app: Application = express();
@@ -19,3 +20,10 @@ app.use(server.router);
 app.listen(port, () => {
    console.log('Now listening on port ' + port);
 });
+
+
+//Firebase
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: "https://vyzerdb.firebaseio.com"
+})
