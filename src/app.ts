9bca39632eea09fb601305f7ec0e9ece565a1ab8
Lukas Wirth <luke.wirth31@gmail.com>
2020-02-25 16:53:10 +0100

    Ordered import statements

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
+import express from 'express';
 import GuideoServer from "./application/GuideoServer";
 import { GuideController } from "./logic/controllers";
 import { UnitOfWork } from './persistence/inmemory/unitofwork';
 import { GuideEndpoint } from './application/rest/endpoints';
-import express from 'express';
 import { IDataInitializer } from './core/contracts';
 import { InMemoryDataInitializer } from './persistence/initializers';
 
@@ -22,9 +22,9 @@ console.log('> added data to repositories');
 
 const server: GuideoServer = new GuideoServer(
     // port
-    port, 
+    port,
     // routables
-    [ new GuideEndpoint(new GuideController(unitOfWork)) ]
+    [new GuideEndpoint(new GuideController(unitOfWork))]
 );
 
 server.app.use(express.static('D:\\Projects\\guideo_backend\\public'));
