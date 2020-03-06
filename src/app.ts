5bab885caf40d8802fb1c57e82934ffab8795d08
LeebAlexander <alexander.leeb11@gmail.com>
2020-03-06 11:45:15 +0100

    added Firebase-queries in Repositories (not tested yet)

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,34 @@
 import express from 'express';
 import { GuideoServer } from './application/GuideoServer';
 import { GuideController } from "./logic/controllers";
-import { UnitOfWork } from './persistence/inmemory/unitofwork';
+//import { UnitOfWork } from './persistence/inmemory/unitofwork';
 import { GuideEndpoint } from './application/rest/endpoints';
 import { IDataInitializer } from './core/contracts';
 import { InMemoryDataInitializer } from './persistence/initializers';
+import { UnitOfWork } from './persistence/firebase/unitofwork';
+import * as admin from 'firebase-admin';
 
 const port: number = 3030;
 const enableCors: boolean = true;
-const unitOfWork: UnitOfWork = new UnitOfWork();
+//const unitOfWork: UnitOfWork = new UnitOfWork();
 const dataInitializer: IDataInitializer = new InMemoryDataInitializer();
+
+var serviceAccount = require(__dirname + '..\\..\\vyzerdb-firebase-adminsdk-02032020.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://vyzerdb.firebaseio.com"
+})
+var db = admin.firestore();
+const unitOfWork: UnitOfWork = new UnitOfWork(db);
 
 console.log('> initialize data ...');
 const result: number = dataInitializer.initDataSync();
 console.log(`> ${result} entries was initizialized`);
 
-unitOfWork.users.addRange(dataInitializer.getUsers());
+/*unitOfWork.users.addRange(dataInitializer.getUsers());
 unitOfWork.guides.addRange(dataInitializer.getGuides());
 unitOfWork.tags.addRange(dataInitializer.getTags());
-unitOfWork.ratings.addRange(dataInitializer.getRatings());
+unitOfWork.ratings.addRange(dataInitializer.getRatings());*/
 console.log('> added data to repositories');
 
 const server: GuideoServer = new GuideoServer({
---
+++ src/persistence/firebase/repositories.ts
@@ -0,0 +1,172 @@
+import {IGuideRepository, IRatingRepository, ITagRepository, IUserRepository} from "../../core/contracts";
+import {IGuide, IRating, ITag, IUser} from "../../core/models";
+import admin from 'firebase-admin'
+import { GuideDto } from '../../logic/datatransferobjects';
+
+export class UserRepository implements IUserRepository {
+
+    usersRef: admin.firestore.CollectionReference;
+
+    constructor(private db: admin.firestore.Firestore){
+        this.usersRef = db.collection('users');
+    }
+
+    async getAll(): Promise<IUser[]> {
+        var users: IUser[];
+
+        this.db.collection('users').get()
+            .then((snapshot) => {
+                snapshot.forEach((doc) => {
+                    console.log(doc.id, '=>', doc.data());
+                    //users.push();
+                });
+            })
+            .catch((err) => {
+                console.log('Error getting documents', err);
+            });
+
+
+        throw 'Not Supported yet';
+    }
+
+    async getUserByName(name: string): Promise<IUser> {
+        var user: IUser;
+        var query = this.usersRef.where('name','==',name).get()
+            .then(snapshot => {
+                if(snapshot.empty){
+                    console.log('No matching documents.');
+                    return;
+                }
+                user = (snapshot.docs[0].data().name, snapshot.docs[0].data().password);
+            })
+            .catch(err => {
+                console.log('Error getting documents', err);
+            })
+
+        throw 'Not Supported yet';
+    }
+
+    async add(item: IUser): Promise<void> {
+        var setUser = this.usersRef.add({
+            name: item.name,
+            password: item.password
+        })
+    }
+
+    async addRange(items: IUser[]): Promise<void> {
+        items.forEach(item => {
+            this.add(item);
+        });
+    }
+
+}
+
+export class GuideRepository implements IGuideRepository {
+
+    guidesRef: admin.firestore.CollectionReference;
+
+    constructor(private db: admin.firestore.Firestore){
+        this.guidesRef = db.collection('guides');
+    }
+
+    async getAll(): Promise<IGuide[]> {
+        throw 'Not Supported yet';
+    }
+
+    async getGuideByName(name: string): Promise<IGuide> {
+        throw 'Not Supported yet';
+    }
+
+    async getGuidesOfUser(userName: string): Promise<IGuide[]> {
+        throw 'Not Supported yet';
+    }
+
+    async getGuidesWithTags(tags: ITag[]): Promise<IGuide[]> {
+        throw 'Not Supported yet';
+    }
+
+    async add(item: IGuide): Promise<void> {
+        var setGuide = this.guidesRef.add({
+            name: item.name,
+            description: item.description,
+            tags: item.tags,
+            userName: item.userName,
+            imageLink: item.imageLink
+        })
+    }
+
+    async addRange(items: IGuide[]): Promise<void> {
+        items.forEach(item => {
+            this.add(item);
+        });
+    }
+}
+
+export class TagRepository implements ITagRepository {
+
+    tagsRef: admin.firestore.CollectionReference;
+
+    constructor(private db: admin.firestore.Firestore){
+        this.tagsRef = db.collection('tags');
+    }
+
+    async getAll(): Promise<ITag[]> {
+        throw 'Not Supported yet';
+    }
+
+    async getTagByName(name: string): Promise<ITag | undefined> {
+        throw 'Not Supported yet';
+    }
+
+    async add(item: ITag): Promise<void> {
+        var setTag = this.tagsRef.add({
+            name: item.name
+        })
+    }
+
+    async addRange(items: ITag[]): Promise<void> {
+        items.forEach(item => {
+            this.add(item);
+        });
+    }
+}
+
+export class RatingRepository implements IRatingRepository {
+
+    ratingsRef: admin.firestore.CollectionReference;
+
+    constructor(private db: admin.firestore.Firestore){
+        this.ratingsRef = db.collection('ratings');
+    }
+
+    async getAverageRatingOfGuide(guideName: string): Promise<number> {
+        throw 'Not Supported yet';
+    }
+
+    async getRatingsOfGuide(guideName: string): Promise<IRating[]> {
+        throw 'Not Supported yet';
+    }
+
+    async getRatingsOfUser(userName: string): Promise<IRating[]> {
+        throw 'Not Supported yet';
+    }
+
+    async getAll(): Promise<IRating[]> {
+        throw 'Not Supported yet';
+    }
+
+    async add(item: IRating): Promise<void> {
+        var setRating = this.ratingsRef.add({
+            guideName: item.guideName,
+            userName: item.userName,
+            rating: item.rating
+        })
+    }
+
+    async addRange(items: IRating[]): Promise<void> {
+        items.forEach(item => {
+            this.add(item);
+        });
+    }
+
+}
