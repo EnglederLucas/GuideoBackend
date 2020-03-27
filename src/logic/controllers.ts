618e6dc8ac2d9581dd0388ece28a8f25f0fd0613
AlexanderLeeb <alexander.leeb1@gmx.at>
2020-03-27 11:33:47 +0100

    Added endpoints for DB-Read Requests

--- src/logic/controllers.ts
+++ src/logic/controllers.ts
@@ -1,11 +1,10 @@
 import { IUnitOfWork, IUserVerifier } from '../core/contracts';
 import {GuideDto, UserDto} from "./datatransferobjects";
-import { IUser, IGuide } from '../core/models';
+import { IUser, IGuide, ITag, IRating } from '../core/models';
 
 export class GuideController {
 
-    constructor(
-        private readonly unitOfWork: IUnitOfWork) {
+    constructor(private readonly unitOfWork: IUnitOfWork) {
     }
 
     async getAll(): Promise<GuideDto[]> {
@@ -15,6 +14,21 @@ export class GuideController {
 
     async getGuidesPaged(index: number, size: number): Promise<GuideDto[]> {
         const guides = await this.unitOfWork.guides.getGuidesPaged(index, size);
+        return await this.convertToDto(guides);
+    }
+
+    async getGuidesByName(name: string): Promise<GuideDto[]> {
+        const guides = await this.unitOfWork.guides.getGuidesByName(name);
+        return await this.convertToDto(guides);
+    }
+
+    async getGuidesOfUser(userName: string): Promise<GuideDto[]>{
+        const guides = await this.unitOfWork.guides.getGuidesOfUser(userName);
+        return await this.convertToDto(guides);
+    }
+
+    async getGuidesWithTags(tags: ITag[]): Promise<GuideDto[]>{
+        const guides = await this.unitOfWork.guides.getGuidesWithTags(tags);
         return await this.convertToDto(guides);
     }
 
@@ -42,18 +56,51 @@ export class UserController {
     }
 
     async getAll(): Promise<UserDto[]> {
-        const users = await this.unitOfWork.users.getAll();
-        const result: UserDto[] = [];
+        return await this.unitOfWork.users.getAll();
+    }
 
-        for (const u of users) {
-            const dto: UserDto = new UserDto(u.name, u.email, u.description);
-            result.push(dto);
-        }
-
-        return result;
+    async getUserByName(name: string): Promise<UserDto>{
+        return await this.unitOfWork.users.getUserByName(name);
     }
 
     registerUser(user : IUser): Promise<void> {
         return this.userVerifier.registerUser(user);
     }
+}
+
+export class TagController {
+
+    constructor(private readonly unitOfWork: IUnitOfWork) {
+    }
+
+    async getAll(): Promise<ITag[]> {
+        return await this.unitOfWork.tags.getAll();
+    }
+
+    async getTagByName(name: string): Promise<ITag> {
+        return await this.unitOfWork.tags.getTagByName(name);
+    }
+}
+
+export class RatingController {
+
+    constructor(private readonly unitOfWork: IUnitOfWork){
+    }
+    
+    async getAll(): Promise<IRating[]> {
+        return await this.unitOfWork.ratings.getAll();
+    }
+
+    async getAverageRatingOfGuide(guideName: string): Promise<number> {
+        return await this.unitOfWork.ratings.getAverageRatingOfGuide(guideName);
+    }
+
+    async getRatingsOfGuide(guideName: string): Promise<IRating[]> {
+        return await this.unitOfWork.ratings.getRatingsOfGuide(guideName);
+    }
+
+    async getRatingsOfUser(userName: string){
+        return await this.unitOfWork.ratings.getRatingsOfUser(userName);
+    }
+
 }
