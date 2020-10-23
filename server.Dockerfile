087c6396a9e56a79d8573aa5b8a858857a5a7903
Lukas Wirth <luke.wirth31@gmail.com>
2020-10-23 10:31:00 +0200

    fixed merge conflicts

--- server.Dockerfile
+++ server.Dockerfile
@@ -1,23 +1,21 @@
 FROM node AS builder
 
 COPY . .
-RUN npm install
-RUN npm install -g typescript
+RUN npm install && npm install -g typescript
 RUN tsc
 
-FROM node
+FROM node AS orderer
 WORKDIR /app
 COPY --from=builder ./build .
 COPY --from=builder ./public ./public
 COPY --from=builder ./package.json .
 COPY --from=builder ./vyzerdb-736d7-firebase-adminsdk-vqpte-d08dfa582b.json .
 
-# FROM node
-# %WORKDIR /app
-# %COPY --from=orderer ./app .
-ENV CRED_PATH /vyzerdb-736d7-firebase-adminsdk-vqpte-d08dfa582b.json
-ENV PUBLIC_PATH /public
-RUN npm install
-RUN ls -la
+FROM node
+WORKDIR /app
+COPY --from=orderer ./app .
+ENV CRED_PATH=/vyzerdb-736d7-firebase-adminsdk-vqpte-d08dfa582b.json PUBLIC_PATH=/public
+RUN npm install --production
+# RUN ls -la
 EXPOSE 3030
 CMD ["node", "app.js"]
--- src/application/endpoints/trackfile.endpoint.ts
+++ src/application/endpoints/trackfile.endpoint.ts
@@ -1,63 +1,66 @@
 import { BaseEndpoint } from './base.endpoint';
 import multer from 'multer';
-import { rename, exists, mkdir, unlink } from 'fs';
-import { promisify } from 'util';
-import $Log from '../../utils/logger';
-import { $log } from '@tsed/logger';
+import { $Log } from '../../utils/logger';
+import { Files } from '../../utils/async-methods';
 
-export class TrackEndpoint extends BaseEndpoint{
+export class TrackEndpoint extends BaseEndpoint {
+  private tempPath: string;
 
-    private readonly renameAsync = promisify(rename);
-    private readonly existsAsync = promisify(exists);
-    private readonly mkdirAsync = promisify(mkdir);
-    private tempPath: string; 
+  constructor(
+    private trackMasterPath: string,
+    tempPath: string | undefined = undefined
+  ) {
+    super('trackfiles');
 
-    constructor(private trackMasterPath: string, tempPath: string | undefined = undefined) {
-        super('trackfiles');
+    this.tempPath = tempPath === undefined ? trackMasterPath : tempPath;
+  }
 
-        this.tempPath = tempPath === undefined ? trackMasterPath : tempPath;
-    }
+  protected initRoutes(): void {
+    const upload = multer({ dest: this.tempPath });
 
-    protected initRoutes(): void {
-        const upload = multer({ dest: this.tempPath });
+    this.router.post('/upload/', upload.single('track'), async (req, res) => {
+      try {
+        if (!req.file) {
+          res.status(400).send('no track sent!');
+          return;
+        }
+        const userPath = `${this.trackMasterPath}\\${req.query.username}`;
+        const guidePath = `${userPath}\\${req.query.guideId}`;
 
-        this.router.post('/upload/', upload.single('track'), async (req, res) => {
-            try {
-                if (!req.file) {
-                    res.status(400).send('no track sent!');
-                    return;
-                }
-                const userPath = `${this.trackMasterPath}\\${req.query.username}`;
-                const guidePath = `${userPath}\\${req.query.guideId}`;
+        if (!(await Files.existsAsync(userPath)))
+          await Files.mkdirAsync(userPath);
+        if (!(await Files.existsAsync(guidePath)))
+          await Files.mkdirAsync(guidePath);
 
-                if (!await this.existsAsync(userPath)) await this.mkdirAsync(userPath);
-                if (!await this.existsAsync(guidePath)) await this.mkdirAsync(guidePath);
+        const tempPath = req.file.path;
+        const targetPath = `${guidePath}\\${req.file.originalname}`;
 
-                const tempPath = req.file.path;
-                const targetPath = `${guidePath}\\${req.file.originalname}`;
+        // rename/move the stored track to the guides folder
+        await Files.renameAsync(tempPath, targetPath);
 
-                // rename/move the stored track to the guides folder
-                await this.renameAsync(tempPath, targetPath);
+        console.log('targetPath: ' + targetPath);
+        // generate the link for the guide object
+        const trackRoute =
+          '/' +
+          targetPath.substring(targetPath.indexOf('tracks')).replace('\\', '/');
 
-                console.log("targetPath: "+targetPath);
-                // generate the link for the guide object
-                const trackRoute = '/' + targetPath
-                    .substring(targetPath.indexOf('tracks'))
-                    .replace('\\', '/');
+        console.log('Trackroute: ' + trackRoute);
 
-                console.log("Trackroute: "+trackRoute);
+        const fileName = req.file.filename;
+        var mp3Duration = require('mp3-duration');
 
-                const fileName = req.file.filename;
-                var mp3Duration = require('mp3-duration');
+        const trackLength = await mp3Duration(`public\\${trackRoute}`);
+        const resObject = { fileName, trackLength, trackRoute };
 
-                const trackLength = await mp3Duration(`public\\${trackRoute}`);
-                const resObject = {fileName, trackLength, trackRoute};
-
-                res.status(200).send(resObject);
-            } catch(err) {
-                res.status(500).contentType('text/plain').send('Oops! An error occured, while storing the track\n error:' + err);
-            }
-        });
-    }
-
+        res.status(200).send(resObject);
+      } catch (err) {
+        res
+          .status(500)
+          .contentType('text/plain')
+          .send(
+            'Oops! An error occured, while storing the track\n error:' + err
+          );
+      }
+    });
+  }
 }
