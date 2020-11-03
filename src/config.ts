142f205d9aa4d521eebf953e065abf0079dc18d9
Lukas Wirth <luke.wirth31@gmail.com>
2020-11-03 13:52:02 +0100

    minor changes

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,7 @@
 export default {
-  port: parseInt(process.env.PORT ?? '3030'),
-  dbUrl: process.env.DB_URL,
-  dbName: process.env.DB_NAME ?? 'guideo',
-  credPath:
-    process.env.CRED_PATH ??
-    '/vyzerdb-736d7-firebase-adminsdk-vqpte-d08dfa582b.json',
-  publicPath: process.env.PUBLIC_PATH ?? '/public',
+	port: parseInt(process.env.PORT ?? '3030'),
+	dbUrl: process.env.DB_URL,
+	dbName: process.env.DB_NAME ?? 'guideo',
+	credPath: process.env.CRED_PATH ?? '/vyzerdb-736d7-firebase-adminsdk-vqpte-d08dfa582b.json',
+	publicPath: process.env.PUBLIC_PATH ?? '/public',
 };
